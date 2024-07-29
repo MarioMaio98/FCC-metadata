510c0f82d4a32c2902e9d5bd29b97c1aba1a0573
Mario maio <mariomaio92@gmail.com>
2024-07-29 16:49:23 +0000

    Exercises completed

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer(); // Initialize multer without a specific storage
 
-var app = express();
+const app = express();
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+  
+  //risposta in json in seguito all'upload in jpeg
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+  console.log('Your app is listening on port ' + port);
 });
